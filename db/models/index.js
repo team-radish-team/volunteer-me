f2d495296ed210be9a086aec044dd86a70dda0c2
Lihan Chen <lihan.chen49@gmail.com>
2019-11-25 17:10:41 -0600

    VolunteerEvent model is creating neo4j nodes, but are not unique nodes with collected relationships"

--- db/models/index.js
+++ db/models/index.js
@@ -4,12 +4,14 @@ const Volunteer = require('./volunteer')
 const Category = require('./category')
 const VolunteerEvent = require('./VolunteerEvent')
 
-Volunteer.belongstoMany(Event, {through: VolunteerEvent})
+Volunteer.belongsToMany(Event, {through: VolunteerEvent})
+Event.belongsToMany(Volunteer, {through: VolunteerEvent})
 
 Organization.hasMany(Event)
 Event.belongsTo(Organization)
 
 Volunteer.belongsToMany(Category, {through: 'VolunteerCategory'})
+Category.belongsToMany(Volunteer, {through: 'VolunteerCategory'})
 
 Category.hasMany(Organization)
 Organization.belongsTo(Category)
--- script/seed.js
+++ script/seed.js
@@ -1,6 +1,134 @@
 const db = require('../db/db')
 const faker = require('faker')
-const {Category, Event, Organization, Volunteer} = require('../db/index')
+const {
+  Category,
+  Event,
+  Organization,
+  Volunteer,
+  VolunteerEvent
+} = require('../db/index')
+
+const dummyVolunteers = [
+  {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: 'linda@linda.com',
+    phone: faker.phone.phoneNumber(),
+    interests: ['animals', 'youth'],
+    profilePic: faker.image.avatar(),
+    password: '12345'
+  },
+  {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: 'steve@steve.com',
+    phone: faker.phone.phoneNumber(),
+    interests: ['animals', 'advocacy', 'education'],
+    profilePic: faker.image.avatar(),
+    password: '12345'
+  }
+]
+
+const dummyEvents = [
+  {
+    title: 'Wag upon a Time',
+    startTime: (datePtr = faker.date
+      .future(0.02, this.createdAt)
+      .setHours((hourNum = faker.random.number({min: 8, max: 20})), 0, 0, 0)),
+    endTime: new Date(datePtr).setHours(
+      hourNum + faker.random.number({min: 1, max: 3}),
+      0,
+      0,
+      0
+    ),
+    address: '1501 E 83rd Pl',
+    description: 'Come adopt some rescue dogs!',
+    volunteerTargetNum: faker.random.number({min: 3, max: 25})
+  },
+  {
+    title: 'Voting Literacy 5K',
+    startTime: (datePtr = faker.date
+      .future(0.02, this.createdAt)
+      .setHours((hourNum = faker.random.number({min: 8, max: 20})), 0, 0, 0)),
+    endTime: new Date(datePtr).setHours(
+      hourNum + faker.random.number({min: 1, max: 3}),
+      0,
+      0,
+      0
+    ),
+    address: '1434 N Parkside Ave',
+    description:
+      'Encourage youths to vote by educating them on their options in this 5K.',
+    volunteerTargetNum: faker.random.number({min: 3, max: 25})
+  },
+  {
+    title: 'Dashing Cats with Whiskers',
+    startTime: (datePtr = faker.date
+      .future(0.02, this.createdAt)
+      .setHours((hourNum = faker.random.number({min: 8, max: 20})), 0, 0, 0)),
+    endTime: new Date(datePtr).setHours(
+      hourNum + faker.random.number({min: 1, max: 3}),
+      0,
+      0,
+      0
+    ),
+    address: '1839 N Richmond St',
+    description:
+      'Whiskers is an organization that helps families of cats stay together with fosters willing to accomodate groups of cats.',
+    volunteerTargetNum: faker.random.number({min: 3, max: 25})
+  },
+  {
+    title: 'Kids and Kits',
+    startTime: (datePtr = faker.date
+      .future(0.02, this.createdAt)
+      .setHours((hourNum = faker.random.number({min: 8, max: 20})), 0, 0, 0)),
+    endTime: new Date(datePtr).setHours(
+      hourNum + faker.random.number({min: 1, max: 3}),
+      0,
+      0,
+      0
+    ),
+    address: '9535 S Loomis St',
+    description:
+      'Come join for free kitten socialization with kids to encourage families expecting to socialize their kittens to play with their future human siblings.',
+    volunteerTargetNum: faker.random.number({min: 3, max: 25})
+  },
+  {
+    title: 'Tying Your Shoes',
+    startTime: (datePtr = faker.date
+      .future(0.02, this.createdAt)
+      .setHours((hourNum = faker.random.number({min: 8, max: 20})), 0, 0, 0)),
+    endTime: new Date(datePtr).setHours(
+      hourNum + faker.random.number({min: 1, max: 3}),
+      0,
+      0,
+      0
+    ),
+    address: '1119 E 46th St',
+    description:
+      "Come to the local library as we read popular children's book Tying your Shoes and teach best methods to teach everyday tasks to children with learning disabilities.",
+    volunteerTargetNum: faker.random.number({min: 3, max: 25})
+  }
+]
+
+// const testSeed = async () => {
+//   try {
+//     await db.sync({force: true})
+//     const seededVolunteers = await Promise.all(
+//       dummyVolunteers.map(volunteer => Volunteer.create(volunteer))
+//     )
+//     const seededEvents = await Promise.all(
+//       dummyEvents.map(event => Event.create(event))
+//     )
+//     let volun = seededVolunteers[0]
+//     volun.addEvent(seededEvents[0])
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// testSeed()
+
 const {
   organizationNames,
   missionStatements,
@@ -59,6 +187,26 @@ const createVolunteer = async () => {
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
+
+  const seededVolunteers = await Promise.all(
+    dummyVolunteers.map(volunteer => Volunteer.create(volunteer))
+  )
+  const seededEvents = await Promise.all(
+    dummyEvents.map(event => Event.create(event))
+  )
+
+  let volun = seededVolunteers[0]
+  volun.addEvent(seededEvents)
+
+  let volun2 = seededVolunteers[1]
+  volun2.addEvent(seededEvents[1])
+
+  // await VolunteerEvent.createHasAttended(1, 1)
+  // await VolunteerEvent.createHasAttended(1, 2)
+  // await VolunteerEvent.createHasAttended(1, 3)
+  // await VolunteerEvent.createHasAttended(1, 4)
+  // await VolunteerEvent.createHasAttended(1, 5)
+  // await VolunteerEvent.createHasAttended(2, 2)
 
   for (let i = 0; i < categories.length; i++) {
     await Category.create({
