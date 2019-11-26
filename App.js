647ac7d5bd8a683048a72ce389713468173210ff
Andrew Pratt <apratt957@gmail.com>
2019-11-26 10:54:32 -0600

    commit

--- App.js
+++ App.js
@@ -3,29 +3,11 @@ import * as Font from 'expo-font'
 import React, {useState} from 'react'
 import {Platform, StatusBar, StyleSheet, View} from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
-import {
-  createAppContainer,
-  createStackNavigator,
-  createBottomTabNavigator
-} from 'react-navigation'
+import {createAppContainer} from 'react-navigation'
 import ReactMap from './components/Map'
 import {Provider} from 'react-redux'
 import store from './store'
-
-import VolunteerList from './components/VolunteerList'
-import HomeScreen from './components/HomeScreen'
-import EventList from './components/EventList'
-
-const VolunteerTabs = createBottomTabNavigator(
-  {
-    Events: EventList,
-    Map: ReactMap,
-    Profile: VolunteerList
-  },
-  {
-    initialRouteName: 'Events'
-  }
-)
+import {VolunteerTabs} from './navigation/navigators'
 
 const AppContainer = createAppContainer(VolunteerTabs)
 
